860dd3f45748abc9ad742fabaef7fce25a821002
AzlanAftabSadiq <349389189@gapps.yrdsb.ca>
2025-11-07 14:29:41 -0500

    3 lives System

--- Bomb.java
+++ Bomb.java
@@ -2,6 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Bomb extends Actor
 {
+    int livesNum = 3;
+    
     public void act()
     {
         move(-10);
@@ -11,6 +13,11 @@ public class Bomb extends Actor
         }
         
         if(isTouching(Hero.class)){
+            livesNum -= 1;
+            resetBomb();
+        }
+        
+        if(livesNum == 0){
             GameOver gameOver = new GameOver();
             getWorld().addObject(gameOver, 300, 200);
             getWorld().removeObject(this);
