9275f10a0b888aee5aef7c9ed90411cf631112cf
AzlanAftabSadiq <349389189@gapps.yrdsb.ca>
2025-11-07 13:54:22 -0500

    Add bomb actor

--- MyWorld.java
+++ MyWorld.java
@@ -6,5 +6,8 @@ public class MyWorld extends World {
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        Bomb bomb = new Bomb ();
+        addObject(bomb, 600, 100);
     }
 }
